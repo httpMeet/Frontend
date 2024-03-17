04be479c59e2f4ef955f547d1c3f30d3803e278f
Meet Gandhi <142320206+httpMeet@users.noreply.github.com>
2024-03-17 18:00:27 +0530

    Changes

--- Link Saver/index.js
+++ Link Saver/index.js
@@ -1,6 +1,7 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
@@ -15,6 +16,12 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
     renderLeads()
 })
 
